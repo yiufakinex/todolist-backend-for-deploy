934f918ed88b30052e8fc1cb65f3c5d4198218b0
Fa Kin Yiu, Franklin <lifeaskrystal@hotmail.com>
2024-06-11 00:14:29 -0600

    Modified Webconfig

--- src/main/java/com/franklin/backend/WebConfig.java
+++ src/main/java/com/franklin/backend/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(@NonNull CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://localhost:5173, https://todolist-frontend-cnxl.onrender.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
